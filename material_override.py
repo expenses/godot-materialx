d3cd7a5f729bfa60d82cff23277609d6a94dd8d4
Ashley Ruglys <ashley.ruglys@gmail.com>
2023-09-08 16:54:52 +1200

    Remove dependency on the python MaterialX lib

--- material_override.py
+++ material_override.py
@@ -1,6 +1,5 @@
 # coding: utf-8
-from pxr import Usd, UsdGeom
-import MaterialX as mx
+from pxr import Usd, UsdGeom, UsdShade, Sdf
 import sys
 
 usd_filename = sys.argv[1]
@@ -12,31 +11,28 @@ source_stage = Usd.Stage.Open(usd_filename)
 up_axis = source_stage.GetMetadata("upAxis")
 meters_per_unit = source_stage.GetMetadata("metersPerUnit")
 
-# extract the name of the materialx material.
+# Open the materialx file as a usd and extract the name of the material.
 
-mat = mx.createDocument()
-
-mx.readFromXmlFile(mat, materialx_filename)
-
-file_materials = mat.getNodes("surfacematerial")
-assert len(file_materials) == 1
-file_material = file_materials[0]
-material_name = file_material.getName()
+matx_stage = Usd.Stage.Open(materialx_filename)
+material_paths = [x.GetPath() for x in matx_stage.Traverse() if UsdShade.Material(x)]
+assert len(material_paths) == 1
+material_path = material_paths[0]
+relative_path = material_path.MakeRelativePath("/MaterialX")
 
 # Create the stage
 
-stage = Usd.Stage.CreateNew('override.usda')
+stage = Usd.Stage.CreateNew("override.usda")
 
 stage.SetMetadata("upAxis", up_axis)
 stage.SetMetadata("metersPerUnit", meters_per_unit)
 
-root = stage.DefinePrim('/root', "Xform")
+root = stage.DefinePrim("/root", "Xform")
 
 stage.SetDefaultPrim(root)
 
 root.GetReferences().AddReference(usd_filename)
 
-scope = stage.DefinePrim('/root/MaterialX', "Scope")
+scope = stage.DefinePrim("/root/MaterialX", "Scope")
 
 scope.GetReferences().AddReference(materialx_filename, "/MaterialX")
 
@@ -45,6 +41,6 @@ mesh_paths = (x.GetPath() for x in stage.Traverse() if UsdGeom.Mesh(x))
 for path in mesh_paths:
     over = stage.OverridePrim(path)
 
-    over.GetRelationship("material:binding").SetTargets(["/root/MaterialX/Materials/" + material_name])
+    over.GetRelationship("material:binding").SetTargets([Sdf.Path("/root/MaterialX").AppendPath(relative_path)])
 
 stage.GetRootLayer().Save()
